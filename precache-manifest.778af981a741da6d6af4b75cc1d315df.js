552b6181c1ba3d989a1d228bce3e2e0276189d60
mayaNg-git <nnhanny2907@gmail.com>
2020-06-25 18:52:28 -0400

    Updates

---
+++ precache-manifest.778af981a741da6d6af4b75cc1d315df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ac035c363c0a413f0b6debceeffb8b",
+    "url": "/friendscontactinfo/index.html"
+  },
+  {
+    "revision": "1a0b3a3306245749c36c",
+    "url": "/friendscontactinfo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "400a65600934e57fc7eb",
+    "url": "/friendscontactinfo/static/css/main.c828007b.chunk.css"
+  },
+  {
+    "revision": "1a0b3a3306245749c36c",
+    "url": "/friendscontactinfo/static/js/2.5ff8d864.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/friendscontactinfo/static/js/2.5ff8d864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "400a65600934e57fc7eb",
+    "url": "/friendscontactinfo/static/js/main.0c36e872.chunk.js"
+  },
+  {
+    "revision": "15f7c89adb10cb77ec5d",
+    "url": "/friendscontactinfo/static/js/runtime-main.b4879896.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/friendscontactinfo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
